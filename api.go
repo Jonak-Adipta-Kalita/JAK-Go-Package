16e117ee03122f29b76d6e1e3b0ffde6801329f1
Jonak-Adipta-Kalita <jonakadiptakalita@gmail.com>
2022-10-24 13:23:00 +0530

    nice!!

--- api.go
+++ api.go
@@ -2,23 +2,38 @@ package jak_go_package
 
 import (
 	"fmt"
+	"io/ioutil"
 	"net/http"
 )
 
 const BASE_URL string = "https://jak_api.p.rapidapi.com"
 
+func makeRequest(rapidApiKey, endpoint string) string {
+	req, _ := http.NewRequest("GET", BASE_URL + endpoint, nil)
+
+	req.Header.Add("X-RapidAPI-Key", rapidApiKey)
+	req.Header.Add("X-RapidAPI-Host", "jak_api.p.rapidapi.com")
+
+	res, resErr := http.DefaultClient.Do(req)
+	
+	if resErr != nil {
+		fmt.Print(resErr.Error())
+	}
+
+	defer res.Body.Close()
+	body, bodyErr := ioutil.ReadAll(res.Body)
+
+	if bodyErr != nil {
+		fmt.Print(bodyErr.Error())
+	}
+
+	return string(body)
+}
+
 type Api struct {
 	RapidAPIKey string
 }
 
-func (x Api) GetJAK() {
-	client := &http.Client{}
-	req, _ := http.NewRequest("GET", BASE_URL + "/jak", nil)
-
-	req.Header.Set("X-RapidAPI-Host", "jak_api.p.rapidapi.com")
-	req.Header.Set("X-RapidAPI-Key", x.RapidAPIKey)
-
-	res, _ := client.Do(req)
-
-	fmt.Print(res.Body)
+func (x Api) GetJAK() string {
+	return makeRequest(x.RapidAPIKey, "/jak")
 }
