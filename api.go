1cbf71aa1fd83196ffb33d20ee90c2c6bc8aee31
Jonak-Adipta-Kalita <jonakadiptakalita@gmail.com>
2022-10-24 18:19:30 +0530

    ben10!!

--- api.go
+++ api.go
@@ -1,39 +1,38 @@
 package jak_go_package
 
 import (
-	"fmt"
+	"encoding/json"
 	"io/ioutil"
 	"net/http"
 )
 
 const BASE_URL string = "https://jak_api.p.rapidapi.com"
 
-func makeRequest(rapidApiKey, endpoint string) string {
-	req, _ := http.NewRequest("GET", BASE_URL + endpoint, nil)
+type Api struct {
+	RapidAPIKey string
+}
 
-	req.Header.Add("X-RapidAPI-Key", rapidApiKey)
+func (x Api) GetBen10() (Ben10, error) {
+	req, _ := http.NewRequest("GET", BASE_URL + "/ben10", nil)
+
+	req.Header.Add("X-RapidAPI-Key", x.RapidAPIKey)
 	req.Header.Add("X-RapidAPI-Host", "jak_api.p.rapidapi.com")
 
 	res, resErr := http.DefaultClient.Do(req)
 	
 	if resErr != nil {
-		fmt.Print(resErr.Error())
+		return Ben10{}, resErr
 	}
 
 	defer res.Body.Close()
 	body, bodyErr := ioutil.ReadAll(res.Body)
 
 	if bodyErr != nil {
-		fmt.Print(bodyErr.Error())
+		return Ben10{}, bodyErr
 	}
 
-	return string(body)
-}
+	var response Ben10
+	json.Unmarshal(body, &response)
 
-type Api struct {
-	RapidAPIKey string
-}
-
-func (x Api) GetJAK() string {
-	return makeRequest(x.RapidAPIKey, "/jak")
+	return response, nil
 }
---
+++ types_ben10.go
@@ -0,0 +1,11 @@
+package jak_go_package
+
+type Ben10 struct {
+	Omnitrix []Omnitrix
+}
+
+type Omnitrix struct {
+	Id    int
+	Name  string
+	Image string
+}
